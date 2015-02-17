a9f08003cabd00d8c4f2dacd3fc92fd752be21a5
sg3510 <sg3510@ic.ac.uk>
2015-02-17 11:25:26 +0000

    bundles test

--- simple_test.py
+++ simple_test.py
@@ -1,11 +1,17 @@
 from dejavu import Dejavu
-import warnings
 import json
+import sys
+sys.path.append('../../Server/')
+import models
+import warnings
 warnings.filterwarnings("ignore")
 
 # load config from a JSON file (or anything outputting a python dictionary)
 with open("dejavu.cnf") as f:
     config = json.load(f)
+
+bundles = models.Training_DB.objects(user = 'sebgrubb', bundle = 'dining_room')
+print bundles
 
 # create a Dejavu instance
 djv = Dejavu(config)
