22935a5c616c135ce219909de4f85a0e89adfe00
sg3510 <sg3510@ic.ac.uk>
2015-02-17 17:23:28 +0000

    testing

--- simple_test.py
+++ simple_test.py
@@ -16,3 +16,10 @@ bundles = models.Training_DB.objects(user = 'sebgrubb', bundle = 'main')
 djv = Dejavu(config)
 djv.fingerprint_bundle(bundles, 3)
 bundles = models.Training_DB.objects(user = 'sebgrubb', bundle = 'dining_room')
+
+
+__PATH__ = "../../Server/website/app/"
+fname = '/static/train_db/user/54808cf080d8ab078e26b8f5/main/out_2014-12-17_16-11-53_pc_80e65026a704.wav'
+from dejavu.recognize import FileRecognizer
+song = djv.recognize(FileRecognizer, __PATH__+fname)
+print song
