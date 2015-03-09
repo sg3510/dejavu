a72e3c632f87bdd71b64734d038d16c003f85394
Seb Grubb <sg3510@ic.ac.uk>
2015-03-09 15:09:43 +0000

    djv settings

--- dejavu/fingerprint.py
+++ dejavu/fingerprint.py
@@ -7,59 +7,64 @@ from scipy.ndimage.morphology import (generate_binary_structure,
 import hashlib
 from operator import itemgetter
 
-IDX_FREQ_I = 0
-IDX_TIME_J = 1
+# Loads settings from the db
+from models import Sys_Settings
+sys_sett = models.Sys_Settings.objects.get()
+
+
+IDX_FREQ_I = sys_sett.dejavu_settings.IDX_FREQ_I
+IDX_TIME_J = sys_sett.dejavu_settings.IDX_FREQ_J
 
 ######################################################################
 # Sampling rate, related to the Nyquist conditions, which affects
 # the range frequencies we can detect.
-DEFAULT_FS = 44100
+DEFAULT_FS = sys_sett.dejavu_settings.DEFAULT_FS
 
 ######################################################################
 # Size of the FFT window, affects frequency granularity
-DEFAULT_WINDOW_SIZE = 4096
+DEFAULT_WINDOW_SIZE = sys_sett.dejavu_settings.DEFAULT_WINDOW_SIZE
 
 ######################################################################
 # Ratio by which each sequential window overlaps the last and the
 # next window. Higher overlap will allow a higher granularity of offset
 # matching, but potentially more fingerprints.
-DEFAULT_OVERLAP_RATIO = 0.5
+DEFAULT_OVERLAP_RATIO = float(sys_sett.dejavu_settings.DEFAULT_OVERLAP_RATIO)
 
 ######################################################################
 # Degree to which a fingerprint can be paired with its neighbors --
 # higher will cause more fingerprints, but potentially better accuracy.
-DEFAULT_FAN_VALUE = 15
+DEFAULT_FAN_VALUE = sys_sett.dejavu_settings.DEFAULT_FAN_VALUE
 
 ######################################################################
 # Minimum amplitude in spectrogram in order to be considered a peak.
 # This can be raised to reduce number of fingerprints, but can negatively
 # affect accuracy.
-DEFAULT_AMP_MIN = 10
+DEFAULT_AMP_MIN = sys_sett.dejavu_settings.DEFAULT_AMP_MIN
 
 ######################################################################
 # Number of cells around an amplitude peak in the spectrogram in order
 # for Dejavu to consider it a spectral peak. Higher values mean less
 # fingerprints and faster matching, but can potentially affect accuracy.
-PEAK_NEIGHBORHOOD_SIZE = 20
+PEAK_NEIGHBORHOOD_SIZE = sys_sett.dejavu_settings.PEAK_NEIGHBORHOOD_SIZE
 
 ######################################################################
 # Thresholds on how close or far fingerprints can be in time in order
 # to be paired as a fingerprint. If your max is too low, higher values of
 # DEFAULT_FAN_VALUE may not perform as expected.
-MIN_HASH_TIME_DELTA = 0
-MAX_HASH_TIME_DELTA = 200
+MIN_HASH_TIME_DELTA = sys_sett.dejavu_settings.MIN_HASH_TIME_DELTA
+MAX_HASH_TIME_DELTA = sys_sett.dejavu_settings.MAX_HASH_TIME_DELTA
 
 ######################################################################
 # If True, will sort peaks temporally for fingerprinting;
 # not sorting will cut down number of fingerprints, but potentially
 # affect performance.
-PEAK_SORT = True
+PEAK_SORT = sys_sett.dejavu_settings.PEAK_SORT
 
 ######################################################################
 # Number of bits to throw away from the front of the SHA1 hash in the
 # fingerprint calculation. The more you throw away, the less storage, but
 # potentially higher collisions and misclassifications when identifying songs.
-FINGERPRINT_REDUCTION = 20
+FINGERPRINT_REDUCTION = sys_sett.dejavu_settings.FINGERPRINT_REDUCTION
 
 def fingerprint(channel_samples, Fs=DEFAULT_FS,
                 wsize=DEFAULT_WINDOW_SIZE,
